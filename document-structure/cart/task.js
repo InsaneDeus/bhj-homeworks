2dce414190f3ad7d44e48e8d2b30d18a1a15eddc
InsaneDeus <insanedeusdesu@mail.ru>
2020-07-31 12:13:48 +0300

    Homeworks dom-structure v2

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -15,25 +15,27 @@ for(let i = 0; i < product.length; i++){
    
 
     product[i].querySelector(".product__add").addEventListener("click", function(){
-        let id = "item" + (i + 1);
-        let valueJ = 0;
-
-        function getValue(){
-            for(let j = 0; j < cartProducts[0].querySelectorAll(".cart__product").length; j++){
-                if(cartProducts[0].querySelectorAll(".cart__product")[j].dataset.id === id){
-                    valueJ = j;
-                    return true;
-                }
+        if(parseInt(product[i].querySelector(".product__quantity-value").textContent) > 0){
+            let id = "item" + (i + 1);
+            let valueJ = 0;
+    
+            function getValue(){
+                for(let j = 0; j < cartProducts[0].querySelectorAll(".cart__product").length; j++){
+                    if(cartProducts[0].querySelectorAll(".cart__product")[j].dataset.id === id){
+                        valueJ = j;
+                        return true;
+                    }
+                };
+                return false;
             };
-            return false;
-        }
-
-        if(getValue()){
-            cartProducts[0].querySelectorAll(".cart__product")[valueJ].querySelector(".cart__product-count").textContent = parseInt(product[i].querySelector(".product__quantity-value").textContent) + parseInt(cartProducts[0].querySelectorAll(".cart__product")[valueJ].querySelector(".cart__product-count").textContent)
-        }
-        else{
-            cartProducts[0].innerHTML += '<div class="cart__product" data-id="' + id + '"><img class="cart__product-image" src="' + product[i].querySelector(".product__image").getAttribute("src") + '"><div class="cart__product-count">' + product[i].querySelector(".product__quantity-value").textContent + '</div></div>';
-        }
+    
+            if(getValue()){
+                cartProducts[0].querySelectorAll(".cart__product")[valueJ].querySelector(".cart__product-count").textContent = parseInt(product[i].querySelector(".product__quantity-value").textContent) + parseInt(cartProducts[0].querySelectorAll(".cart__product")[valueJ].querySelector(".cart__product-count").textContent)
+            }
+            else{
+                cartProducts[0].innerHTML += '<div class="cart__product" data-id="' + id + '"><img class="cart__product-image" src="' + product[i].querySelector(".product__image").getAttribute("src") + '"><div class="cart__product-count">' + product[i].querySelector(".product__quantity-value").textContent + '</div></div>';
+            };
+        };
     });
 };
 
--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,17 +2,24 @@ const button = document.getElementById("tasks__add");
 const input = document.getElementById("task__input");
 const taskList = document.getElementById("tasks__list");
 
+
 button.addEventListener("click", function(e) {
     e.preventDefault();
 
     if(input.value !== ""){
-        taskList.innerHTML = taskList.innerHTML + '<div  class="task"><div class="task__title">' + input.value + '</div><a href="#"class="task__remove"> &times;</a></div>';
+       // taskList.innerHTML = taskList.innerHTML + '<div  class="task"><div class="task__title">' + input.value + '</div><a href="#"class="task__remove"> &times;</a></div>';
 
-        for(let i = 0; i < taskList.querySelectorAll(".task__remove").length; i++){
-            taskList.querySelectorAll(".task__remove")[i].addEventListener("click", function(){
-                taskList.querySelectorAll(".task")[i].remove();
-            });
-        };
+        taskList.insertAdjacentHTML("beforeend", '<div  class="task"><div class="task__title">' + input.value + '</div><a href="#"class="task__remove" onclick = ""> &times;</a></div>');
+
+        let num = taskList.querySelectorAll(".task__remove").length - 1;
+
+        taskList.querySelectorAll(".task__remove")[num].addEventListener("click", function(e){
+            e.preventDefault();
+        
+            event.target.parentElement.remove();
+        });
+
+        input.value = "";
     };
     
 });
